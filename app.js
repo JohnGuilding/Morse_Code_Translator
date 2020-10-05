04f943641ae05fa1e45669231621c365091231ea
John Guilding <jwguilding@gmail.com>
2020-10-05 09:34:11 +0100

    functionality refactored and fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // As an extension it should be able to translate both ways.
 
 // 1. store alphabet
-// 2. sotre morse code
+// 2. store morse code
 // 3. listener on the btn listening for the click
 // 4. ON CLICK -> run a function -> convert english to morse
 // 5. 'convert english to morse' -> get the letter and find the corresponding morse
@@ -13,106 +13,138 @@
 // 8. refresh button with event listener
 
 // const morseArray = [
-//     {letter: 'a', morse: '.-'},
-//     {letter: 'b', morse: '-...'},
-//     {letter: 'c', morse: '-.-.'},
-//     {letter: 'd', morse: '-..'},
-//     {letter: 'e', morse: '.'},
-//     {letter: 'f', morse: '..-.'},
-//     {letter: 'g', morse: '--.'},
-//     {letter: 'h', morse: '....'},
-//     {letter: 'i', morse: '..'},
-//     {letter: 'j', morse: '.---'},
-//     {letter: 'k', morse: '-.-'},
-//     {letter: 'l', morse: '.-..'},
-//     {letter: 'm', morse: '--'},
-//     {letter: 'n', morse: '-.'},
-//     {letter: 'o', morse: '---'},
-//     {letter: 'p', morse: '.--.'},
-//     {letter: 'q', morse: '--.-'},
-//     {letter: 'r', morse: '.-.'},
-//     {letter: 's', morse: '...'},
-//     {letter: 't', morse: '-'},
-//     {letter: 'u', morse: '..-'},
-//     {letter: 'v', morse: '...-'},
-//     {letter: 'w', morse: '.--'},
-//     {letter: 'x', morse: '-..-'},
-//     {letter: 'y', morse: '-.--'},
-//     {letter: 'z', morse: '--..'},
-//     {letter: ' ', morse: ' '}
+//     {'a' : '.-'},
+//     {'b' : '-...'},
+//     {'c' : '-.-.'},
+//     {'d' : '-..'},
+//     {'e' : '.'},
+//     {'f' : '..-.'},
+//     {'g' : '--.'},
+//     {'h' : '....'},
+//     {'i' : '..'},
+//     {'j' : '.---'},
+//     {'k' : '-.-'},
+//     {'l' : '.-..'},
+//     {'m' : '--'},
+//     {'n' : '-.'},
+//     {'o' : '---'},
+//     {'p' : '.--.'},
+//     {'q' : '--.-'},
+//     {'r' : '.-.'},
+//     {'s' : '...'},
+//     {'t' : '-'},
+//     {'u' : '..-'},
+//     {'v' : '...-'},
+//     {'w' : '.--'},
+//     {'x' : '-..-'},
+//     {'y' : '-.--'},
+//     {'z' : '--..'},
+//     {' ' : ' '}
 // ];
 
-const morseArray = [
-    {'a' : '.-'},
-    {'b' : '-...'},
-    {'c' : '-.-.'},
-    {'d' : '-..'},
-    {'e' : '.'},
-    {'f' : '..-.'},
-    {'g' : '--.'},
-    {'h' : '....'},
-    {'i' : '..'},
-    {'j' : '.---'},
-    {'k' : '-.-'},
-    {'l' : '.-..'},
-    {'m' : '--'},
-    {'n' : '-.'},
-    {'o' : '---'},
-    {'p' : '.--.'},
-    {'q' : '--.-'},
-    {'r' : '.-.'},
-    {'s' : '...'},
-    {'t' : '-'},
-    {'u' : '..-'},
-    {'v' : '...-'},
-    {'w' : '.--'},
-    {'x' : '-..-'},
-    {'y' : '-.--'},
-    {'z' : '--..'},
-    {' ' : ' '}
-];
+const englishArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// console.log(morseArray);
-
-// console.log(Object.entries(morseArray));
+const morseArray = ['.-', '-...', '-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..'];
 
 
 const translation = document.querySelector('.translation');
 
 ////////////////////////////// TRANSLATE FUNCTION //////////////////////////////
 
-// GET USER INPUT FROM FORM //
-const getValue = () => {
-    const form = document.querySelector("form");
-    output = form.morseCode.value;
+const form = document.querySelector(".form");
+const submitBtn = document.querySelector(".submitBtn");
 
-    // SETS INPUT TO ARRAY AND SPLITS STRING WITH SPREAD SYNTAX //
+// const getValue = () => {
+    
+//     let output = form.morseCode.value;
+//     console.log(output);
+//     return output;
+// }
+//     console.log(output);
+
+
+
+
+submitBtn.addEventListener('click', () => {
+    const output = form.morseCode.value;
+    // console.log(output);
+
     const inputArray = [...output];
+    // console.log(inputArray);
 
-    // IF INPUT CHARACTERS MATCH LETTERS ARRAY DO SOMETHING... //
-    morseArray.forEach((objectIteration) => {
-        // const lettersArray = Object.keys(letter);
+    inputArray.forEach((inputLetter) => {
 
-        Object.entries(objectIteration).forEach(([key, value]) => {
-            // console.log(`${key}: ${value}`);
-            // return key;
-
-            inputArray.forEach((inputLetter) => {
-                if (inputLetter == key) {
-                
-                    // String(value);
-                    console.log(typeof value);
-                    translation.innerHTML += '  ' + value;
-                    
-                }
-            });
-
+        englishArray.forEach((arrayLetter) => {
+            if (inputLetter == arrayLetter) {
+                console.log(englishArray.indexOf(arrayLetter));
+                morseArray.forEach((arrayMorse) => {
+                    if (morseArray.indexOf(arrayMorse) == englishArray.indexOf(arrayLetter)) {
+                        console.log(arrayMorse);
+                    }
+                });
+            }
         });
-
-
-        
     });
-}
+});
+
+
+
+// for(let i = 0; i < morseArray.length; i++) {
+//     if(letter == morseArray[i]) {
+//         console.log('oh yes oh yes');
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // GET USER INPUT FROM FORM //
+// const getValue = () => {
+//     const form = document.querySelector("form");
+//     output = form.morseCode.value;
+
+//     // SETS INPUT TO ARRAY AND SPLITS STRING WITH SPREAD SYNTAX //
+//     const inputArray = [...output];
+
+//     // IF INPUT CHARACTERS MATCH LETTERS ARRAY DO SOMETHING... //
+//     morseArray.forEach((objectIteration) => {
+//         // const lettersArray = Object.keys(letter);
+
+//         Object.entries(objectIteration).forEach(([key, value]) => {
+//             // console.log(`${key}: ${value}`);
+//             // return key;
+
+//             inputArray.forEach((inputLetter) => {
+//                 if (inputLetter == key) {
+                
+//                     // String(value);
+//                     console.log(typeof value);
+//                     translation.innerHTML += '  ' + value;
+                    
+//                 }
+//             });
+
+//         });
+
+//     });
+// }
+
+
+
 
     // morseArray.forEach((object) => {
     //     let newArray = Object.entries(keys);
