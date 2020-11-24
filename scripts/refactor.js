30f4e9bde3ba494ae496df65fc9d3e1a8903f634
John Guilding <jwguilding@gmail.com>
2020-11-24 15:54:10 +0000

    encode and decode refactor working

--- scripts/refactor.js
+++ scripts/refactor.js
@@ -39,47 +39,45 @@ const translationDataObject = {
 	" ": "   ",
 };
 
-// const translationDataObject = {
-// 	".-": "a", 
-// 	"-...": "b",
-// 	"-.-.": "c",
-// 	"-..": "d",
-// 	".": "e", 
-// 	"..-.": "f", 
-// 	"--.": "g", 
-// 	"....": "h", 
-// 	"..": "i", 
-// 	".---": "j", 
-// 	"-.-": "k", 
-// 	".-..": "l", 
-// 	"--": "m", 
-// 	"-.": "n", 
-// 	"---": "o", 
-// 	".--.": "p", 
-// 	"--.-": "q", 
-// 	".-.": "r", 
-// 	"...": "s", 
-// 	"-": "t", 
-// 	"..-": "u", 
-// 	"...-": "v", 
-// 	".--": "w", 
-// 	"-..-": "x", 
-// 	"-.--": "y", 
-// 	"--..": "z", 
-// 	".----": "1", 
-// 	"..---": "2", 
-// 	"...--": "3", 
-// 	"....-": "4", 
-// 	".....": "5", 
-// 	"-....": "6", 
-// 	"--...": "7", 
-// 	"---..": "8", 
-// 	"----.": "9", 
-// 	"-----": "0", 
-// 	"   ": " ",
-// };
-
-
+const translationDataObjectm = {
+	".-": "a", 
+	"-...": "b",
+	"-.-.": "c",
+	"-..": "d",
+	".": "e", 
+	"..-.": "f", 
+	"--.": "g", 
+	"....": "h", 
+	"..": "i", 
+	".---": "j", 
+	"-.-": "k", 
+	".-..": "l", 
+	"--": "m", 
+	"-.": "n", 
+	"---": "o", 
+	".--.": "p", 
+	"--.-": "q", 
+	".-.": "r", 
+	"...": "s", 
+	"-": "t", 
+	"..-": "u", 
+	"...-": "v", 
+	".--": "w", 
+	"-..-": "x", 
+	"-.--": "y", 
+	"--..": "z", 
+	".----": "1", 
+	"..---": "2", 
+	"...--": "3", 
+	"....-": "4", 
+	".....": "5", 
+	"-....": "6", 
+	"--...": "7", 
+	"---..": "8", 
+	"----.": "9", 
+	"-----": "0", 
+	"   ": " ",
+};
 
 // const message = "hello";
 
@@ -96,18 +94,18 @@ const translateToMorse = (input) => {
     console.log(word);
 }
 
-// const translateToEnglish = (input) => {
-//     const word = input
-//     .split(' ')
-//     .map(
-//         (character) => character
-//         .split(' ')
-//         .map((b) => translationDataObjectm[b]
-//     ).join('')
-//     ).join(' ');
-//     console.log('to English');
-//     console.log(word);
-// }
+const translateToEnglish = (input) => {
+    const word = input
+    .split('  ')
+    .map(
+        (word) => word
+        .split(' ')
+        .map((character) => translationDataObjectm[character]
+    ).join('')
+    ).join(' ');
+    console.log('to English');
+    console.log(word);
+}
 
 translateToMorse('hello mate')
-// translateToEnglish('.... . .-.. .-.. --- -- .- - .');
+translateToEnglish('.... . .-.. .-.. ---   -- .- - .');
