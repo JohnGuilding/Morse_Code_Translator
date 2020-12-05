6a93f871d8f5f3270abbbd845c7fe81d1ec32754
JohnGuilding <jwguilding@gmail.com>
2020-12-05 22:06:30 +0000

    wrote valid tests

--- scripts/translate.test.js
+++ scripts/translate.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("@jest/globals");
 
-import { translateEnglish } from "./translate";
+import { translateToMorse, translateToEnglish } from "./translate";
 
 // Valid inputs
 it ('should be 1', () => {
@@ -8,13 +8,30 @@ it ('should be 1', () => {
 });
 
 it('should translate to .-', () => {
-    const translation = translateEnglish('a')
+    const translation = translateToMorse('A')
     expect(translation).toBe('.-')
-})
+});
 
-// it('should translate a to .-', () => {
-//     const translation = translateToMorse('a');
-//     expect(translation).toBe('.-')
-// })
+it('should translate to .... . .-.. .-.. ---   .-- --- .-. .-.. -..', () => {
+    const translation = translateToMorse('hello world')
+    expect(translation).toBe('.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
+});
+
+it('should translate to .... --- .--   -- ..- -.-. ....   .-- --- --- -..   .-- --- ..- .-.. -..   .-   .-- --- --- -.. -.-. .... ..- -.-. -.-   -.-. .... ..- -.-. -.-   .. ..-.   .-   .-- --- --- -.. -.-. .... ..- -.-. -.-   -.-. --- ..- .-.. -..   -.-. .... ..- -.-. -.-   .-- --- --- -..', () => {
+    const translation = translateToMorse('how much wood would a woodchuck chuck if a woodchuck could chuck wood')
+    expect(translation).toBe('.... --- .--   -- ..- -.-. ....   .-- --- --- -..   .-- --- ..- .-.. -..   .-   .-- --- --- -.. -.-. .... ..- -.-. -.-   -.-. .... ..- -.-. -.-   .. ..-.   .-   .-- --- --- -.. -.-. .... ..- -.-. -.-   -.-. --- ..- .-.. -..   -.-. .... ..- -.-. -.-   .-- --- --- -..')
+});
+
+it('should translate to a', () => {
+    const translation = translateToEnglish('.-')
+    expect(translation).toBe('a')
+});
+
+it('should translate to hello world', () => {
+    const translation = translateToEnglish('.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
+    expect(translation).toBe('hello world')
+});
+
 // Invalid inputs
+
 // Null/no inputs
